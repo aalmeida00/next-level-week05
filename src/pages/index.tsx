e31de1a6ad6baffee1c72ab0045f9e72f5923402
aalmeida00 <andre-silva78@hotmail.com>
2021-04-24 14:45:44 -0300

    feat: change fetch to axios and configure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,33 @@
-export default function Home(props) {
+import { GetStaticProps } from 'next';
+import { api } from '../services/api';
+
+type HomeProps = {
+  episodes: Episode[];
+};
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+};
+
+export default function Home(props: HomeProps) {
   return (
     <>
       <h1>index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
     </>
   );
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    },
+  });
 
   return {
     props: {
@@ -16,4 +35,4 @@ export async function getStaticProps() {
     },
     revalidate: 60 * 60 * 8,
   };
-}
+};
