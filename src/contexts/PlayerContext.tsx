239708e39cca1891d7a183f5e1021694cd6ae8e1
aalmeida00 <andre-silva78@hotmail.com>
2021-04-25 19:49:46 -0300

    refactor: player context typescript typos defined

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, ReactNode, useState } from 'react';
 
 type Episode = {
   title: string;
@@ -17,14 +17,20 @@ type PlayerContextData = {
   togglePlay: () => void;
 };
 
+type PlayerContextProviderProps = {
+  children: ReactNode;
+};
+
 export const playerContext = createContext({} as PlayerContextData);
 
-export function PlayerContextProvider({ children }) {
+export function PlayerContextProvider({
+  children,
+}: PlayerContextProviderProps) {
   const [episodeList, setEpisodeList] = useState([]);
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
 
-  function play(episode) {
+  function play(episode: Episode) {
     setEpisodeList([episode]);
     setCurrentEpisodeIndex(0);
     setIsPlaying(true);
